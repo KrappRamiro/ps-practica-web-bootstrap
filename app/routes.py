5967b828bc80b0ce0783cf163c8a1c7c972c41f6
Krapp Ramiro <krappramiro@disroot.org>
2023-06-20 21:10:22 -0300

    add sass and bootstrap

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,7 @@ from flask import render_template, request, session, jsonify, redirect
 
 @app.route('/')
 def index():
-    print("a")
-    return "a"
+    return render_template(
+        "index.html",
+        title="Home",
+    )

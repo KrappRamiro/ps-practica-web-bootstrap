cfa7cd9e1d2b91f31af2f75bcdab153d597356b9
Krapp Ramiro <krappramiro@disroot.org>
2023-06-23 09:17:34 -0300

    fixed frozen flask url generation

--- frozen_flask.py
+++ frozen_flask.py
@@ -1,7 +1,10 @@
 from app import app
-from flask_frozen import Freezer  # Added
+from flask_frozen import Freezer
+
+app.config["FREEZER_BASE_URL"] = "/ps-practica-web-bootstrap/"
 
 freezer = Freezer(app)
+
 
 if __name__ == "__main__":
     freezer.freeze()
